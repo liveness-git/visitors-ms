455949b85b7b323e5b90eda32812b7945cb53e43
RieNishi <r.nishi@liveness.co.jp>
2022-12-26 11:37:01 +0900

    ログ出力機能の追加

--- config/log.js
+++ config/log.js
@@ -10,20 +10,104 @@
  * https://sailsjs.com/docs/concepts/logging
  */
 
+const { createLogger, format, transports } = require("winston");
+const { version } = require("../package.json");
+const { SPLAT } = require("triple-beam");
+const { isObject } = require("@sailshq/lodash");
+const moment = require("moment-timezone");
+
+const logLevel = "info";
+const sailsLogLevels = {
+  error: 0,
+  warn: 1,
+  debug: 2,
+  info: 3,
+  verbose: 4,
+  silly: 5,
+};
+
+function formatObject(param) {
+  try {
+    if (isObject(param)) {
+      return JSON.stringify(param);
+    }
+    return param;
+  } catch (e) {
+    return "";
+  }
+}
+
+const all = format((info) => {
+  const splat = info[SPLAT] || [];
+  const message = formatObject(info.message);
+  const rest = splat.map(formatObject).join(" ");
+  info.message = `${message} ${rest}`;
+  return info;
+});
+
+const localTimeStamp = () => {
+  return moment().format("YYYY/MM/DD HH:mm:ss.SSS");
+};
+
+const customLogger = createLogger({
+  // format: format.combine(
+  //   all(),
+  //   format.label({ label: version }),
+  //   format.timestamp(),
+  //   format.colorize(),
+  //   format.align(),
+  //   format.printf(info => `${info.timestamp} [${info.label}] ${info.level}: ${formatObject(info.message)}`),
+  // ),
+  level: logLevel,
+  levels: sailsLogLevels,
+  transports: [
+    new transports.Console({
+      format: format.combine(
+        all(),
+        format.label({ label: version }),
+        format.timestamp({ format: localTimeStamp }),
+        format.colorize(),
+        format.align(),
+        format.printf(
+          (info) =>
+            `${info.timestamp} [${info.label}] ${info.level}: ${formatObject(
+              info.message
+            )}`
+        )
+      ),
+    }),
+    new transports.File({
+      filename: "visitors.log",
+      maxsize: 1024 * 1024,
+      maxFiles: 9,
+      format: format.combine(
+        all(),
+        format.label({ label: version }),
+        format.timestamp({ format: localTimeStamp }),
+        format.printf(
+          (info) =>
+            `${info.timestamp} [${info.label}] ${info.level}: ${formatObject(
+              info.message
+            )}`
+        )
+      ),
+    }),
+  ],
+});
+
 module.exports.log = {
-
   /***************************************************************************
-  *                                                                          *
-  * Valid `level` configs: i.e. the minimum log level to capture with        *
-  * sails.log.*()                                                            *
-  *                                                                          *
-  * The order of precedence for log levels from lowest to highest is:        *
-  * silly, verbose, info, debug, warn, error                                 *
-  *                                                                          *
-  * You may also set the level to "silent" to suppress all logs.             *
-  *                                                                          *
-  ***************************************************************************/
-
-  // level: 'info'
-
+   *                                                                          *
+   * Valid `level` configs: i.e. the minimum log level to capture with        *
+   * sails.log.*()                                                            *
+   *                                                                          *
+   * The order of precedence for log levels from lowest to highest is:        *
+   * silly, verbose, info, debug, warn, error                                 *
+   *                                                                          *
+   * You may also set the level to "silent" to suppress all logs.             *
+   *                                                                          *
+   ***************************************************************************/
+  custom: customLogger,
+  inspect: false,
+  level: logLevel,
 };
