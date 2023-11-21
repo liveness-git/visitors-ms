174ceb2fe7cd0bf3c59e11a4d657c3dbe11afd12
RieNishi <r.nishi@liveness.co.jp>
2023-11-21 12:23:07 +0900

    キャッシュログModel作成

---
+++ api/models/CacheLog.js
@@ -0,0 +1,47 @@
+/**
+ * CacheLog.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    obtainDatetime: {
+      type: "ref",
+      columnType: "datetime",
+      required: true,
+    },
+    type: {
+      type: "string",
+      isIn: ["event"],
+      required: true,
+    },
+    mode: {
+      type: "string",
+      isIn: ["reset", "patch"],
+      required: true,
+    },
+    start: {
+      type: "ref",
+      columnType: "datetime",
+      required: true,
+    },
+    end: {
+      type: "ref",
+      columnType: "datetime",
+      required: true,
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+};
